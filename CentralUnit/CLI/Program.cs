553cdeddc8409fcc5c104c73b9d10345dccf8cac
CptMalfunction <jan709.psn@gmail.com>
2020-12-28 14:36:22 +0100

    Issue 10: Added interfaces for edge units and basic commandline

---
+++ CentralUnit/CLI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace CLI
+{
+    class Program
+    {
+        public static void Main(string[] args) => CommandLineApplication.Execute<RootCommand>(args);
+    }
+
+    [Command("mgk")]
+    [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+    class RootCommand : CommandBase
+    {
+        protected override int OnExecute(CommandLineApplication app)
+        {
+            Console.WriteLine("This is the root command, it does not do anything");
+            app.ShowHelp();
+            return 1;
+        }
+
+        private static string GetVersion()
+            => typeof(RootCommand).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+    }
+
+    [HelpOption("--help")]
+    abstract class CommandBase
+    {
+        protected abstract int OnExecute(CommandLineApplication app);
+    }
+}
---
+++ CentralUnit/RiderIdUnit/IRiderIdUnit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RiderIdUnit
+{
+    /// <summary>
+    /// A rider id unit will identify riders that are near its sensor. It cannot de precise timing, so an id unit will not report times with its events
+    /// </summary>
+    public interface IRiderIdUnit
+    {
+        /// <summary>
+        /// This event will fire when a rider enters the units sensor range
+        /// </summary>
+        event EventHandler OnRiderId;
+
+        /// <summary>
+        /// This event will fire when a rider leaves sensor range
+        /// </summary>
+        event EventHandler OnRiderExit;
+
+        /// <summary>
+        /// A unit will store which riders it should report about, to avoid reporting riders or loose senders that are near the sensor range
+        /// </summary>
+        void ClearKnownRiders();
+
+        /// <summary>
+        /// Add new riders that the unit should report about.
+        /// </summary>
+        /// <param name="riders">The new riders, Guid should match what the sensor receives. Both sensorId and name should be unkown to the unit</param>
+        void AddKnownRiders(List<(Guid sensorId, string name)> riders);
+
+        /// <summary>
+        /// Avoid further events for this rider. To resume events for this rider they should be added again
+        /// </summary>
+        /// <param name="name"></param>
+        void RemoveKnownRider(string name);
+    }
+}
