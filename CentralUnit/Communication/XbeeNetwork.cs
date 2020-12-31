4bdbf756470a504ed32fa6102d9eaf73384113d1
DESKTOP-U3TUG3O\cdromke <github@cdromke.nl>
2020-12-31 22:13:12 +0100

    Adding Digi Xbee library instead of ZigBeeNet Adding XbeeNetwork and SerialCommunication implementation classes

---
+++ CentralUnit/Communication/XbeeNetwork.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="XbeeSerialCommunication.cs" company="Moto Gymkhana">
+//     Copyright (c) Moto Gymkhana. All rights reserved.
+// </copyright>
+namespace Communication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using XBeeLibrary.Core.Models;
+
+    public class XbeeNetwork
+    {
+
+        private List<XbeeSerialCommunication> devices = new List<XbeeSerialCommunication>();
+
+        private ISerialCommunication communicationChannel;
+
+        public XbeeNetwork(ISerialCommunication communication)
+        {
+
+            this.communicationChannel = communication;
+            this.communicationChannel.ConnectionStateChanged += CommunicationChannel_ConnectionStateChanged;
+            this.communicationChannel.DataReceived += CommunicationChannel_DataReceived;
+            this.communicationChannel.Failure += CommunicationChannel_Failure;
+
+        }
+
+        public XbeeSerialCommunication GetDevice(string address64bit)
+        {
+            XBee64BitAddress address = new XBee64BitAddress(address64bit);
+            XbeeSerialCommunication device = null;
+            if (devices.Any(dev => dev.Xbee64address.Equals(address)))
+            {
+                device = devices.First(dev => dev.Xbee64address.Equals(address));
+            }
+            else
+            {
+                device = new XbeeSerialCommunication(address);
+                devices.Add(device);
+            }
+
+            return device;
+
+        }
+
+        private void CommunicationChannel_Failure(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CommunicationChannel_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CommunicationChannel_ConnectionStateChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CentralUnit/Communication/XbeeSerialCommunication.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="XbeeSerialCommunication.cs" company="Moto Gymkhana">
+//     Copyright (c) Moto Gymkhana. All rights reserved.
+// </copyright>
+namespace Communication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using XBeeLibrary.Core.Models;
+
+    public class XbeeSerialCommunication : ISerialCommunication
+    {
+        public bool Connected => throw new NotImplementedException();
+
+        public XBee64BitAddress Xbee64address { get => xbee64address; }
+
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+        public event EventHandler Failure;
+        public event EventHandler ConnectionStateChanged;
+
+        private XBee64BitAddress xbee64address;
+
+        public XbeeSerialCommunication(XBee64BitAddress address)
+        {
+            this.xbee64address = address;
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(byte[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
