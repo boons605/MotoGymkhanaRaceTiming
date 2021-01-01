c0288beaa9bdf074b406b28f9baac24b036aee32
DESKTOP-U3TUG3O\cdromke <github@cdromke.nl>
2021-01-01 12:47:11 +0100

    Issue 14: Further implementation of XbeeSerialCommunication and XbeeNetwork.

--- CentralUnit/Communication/XbeeNetwork.cs
+++ CentralUnit/Communication/XbeeNetwork.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="XbeeSerialCommunication.cs" company="Moto Gymkhana">
+﻿// <copyright file="XbeeNetwork.cs" company="Moto Gymkhana">
 //     Copyright (c) Moto Gymkhana. All rights reserved.
 // </copyright>
 namespace Communication
@@ -9,54 +9,111 @@ namespace Communication
     using System.Text;
     using XBeeLibrary.Core.Models;
 
+    /// <summary>
+    /// A network of <c>Xbee</c> devices accessed through a single serial communication channel.
+    /// </summary>
     public class XbeeNetwork
     {
-
+        /// <summary>
+        /// List of <c>Xbee</c> devices registered with this network.
+        /// </summary>
         private List<XbeeSerialCommunication> devices = new List<XbeeSerialCommunication>();
 
+        /// <summary>
+        /// The serial communication channel utilized by this network.
+        /// </summary>
         private ISerialCommunication communicationChannel;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XbeeNetwork" /> class.
+        /// </summary>
+        /// <param name="communication">The serial communication channel utilized by this network</param>
         public XbeeNetwork(ISerialCommunication communication)
         {
-
             this.communicationChannel = communication;
-            this.communicationChannel.ConnectionStateChanged += CommunicationChannel_ConnectionStateChanged;
-            this.communicationChannel.DataReceived += CommunicationChannel_DataReceived;
-            this.communicationChannel.Failure += CommunicationChannel_Failure;
-
+            this.communicationChannel.ConnectionStateChanged += this.CommunicationChannel_ConnectionStateChanged;
+            this.communicationChannel.DataReceived += this.CommunicationChannel_DataReceived;
+            this.communicationChannel.Failure += this.CommunicationChannel_Failure;
         }
 
+        /// <summary>
+        /// Gets a device from the network or adds this device to the network.
+        /// </summary>
+        /// <param name="address64bit">The 64-bit address to identify the device, in hex format: (0x)0013A20041BB64A6</param>
+        /// <returns>An <see cref="XbeeSerialCommunication"/> communication channel</returns>
         public XbeeSerialCommunication GetDevice(string address64bit)
         {
             XBee64BitAddress address = new XBee64BitAddress(address64bit);
             XbeeSerialCommunication device = null;
-            if (devices.Any(dev => dev.Xbee64address.Equals(address)))
+            if (this.devices.Any(dev => dev.Xbee64address.Equals(address)))
             {
-                device = devices.First(dev => dev.Xbee64address.Equals(address));
+                device = this.devices.First(dev => dev.Xbee64address.Equals(address));
             }
             else
             {
-                device = new XbeeSerialCommunication(address);
-                devices.Add(device);
+                device = new XbeeSerialCommunication(address, this);
+                this.devices.Add(device);
             }
 
             return device;
-
         }
 
-        private void CommunicationChannel_Failure(object sender, EventArgs e)
+        /// <summary>
+        /// Write data, to be used by <see cref="XbeeSerialCommunication"/> devices.
+        /// </summary>
+        /// <param name="data">The <c>Xbee</c> API data to write to the serial communication channel.</param>
+        internal void Write(byte[] data)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Removes the <see cref="XbeeSerialCommunication"/> device from the network.
+        /// </summary>
+        /// <param name="address">The 64-bit address to identify the device to be removed.</param>
+        internal void CloseXbeeDevice(XBee64BitAddress address)
+        {
+            if (this.devices.Any(dev => dev.Xbee64address.Equals(address)))
+            {
+                this.devices.Remove(this.devices.First(dev => dev.Xbee64address.Equals(address)));
+            }
+        }
+
+        /// <summary>
+        /// Notifies all devices in this network of a failure in the serial communication.
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">EventArgs supplied with the event</param>
+        private void CommunicationChannel_Failure(object sender, EventArgs e)
+        {
+            foreach (XbeeSerialCommunication xbee in this.devices)
+            {
+                xbee.OnFailure();
+            }
+        }
+
+        /// <summary>
+        /// Processes data received from the communication channel.
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">EventArgs supplied with the event</param>
         private void CommunicationChannel_DataReceived(object sender, DataReceivedEventArgs e)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Handles change in communication channel connection state.
+        /// Notifies all devices in this network of the new connection state.
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">EventArgs supplied with the event</param>
         private void CommunicationChannel_ConnectionStateChanged(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            foreach (XbeeSerialCommunication xbee in this.devices)
+            {
+                xbee.OnConnectionStateChanged(this.communicationChannel.Connected);
+            }
         }
     }
 }
--- CentralUnit/Communication/XbeeSerialCommunication.cs
+++ CentralUnit/Communication/XbeeSerialCommunication.cs
@@ -7,32 +7,111 @@ namespace Communication
     using System.Collections.Generic;
     using System.Text;
     using XBeeLibrary.Core.Models;
+    using XBeeLibrary.Core.Packet.Common;
 
+    /// <summary>
+    /// Implementation of ISerialCommunication for an device connected through <c>Xbee</c>.
+    /// </summary>
     public class XbeeSerialCommunication : ISerialCommunication
     {
-        public bool Connected => throw new NotImplementedException();
-
-        public XBee64BitAddress Xbee64address { get => xbee64address; }
-
-        public event EventHandler<DataReceivedEventArgs> DataReceived;
-        public event EventHandler Failure;
-        public event EventHandler ConnectionStateChanged;
-
+        /// <summary>
+        /// The 64-bit address of the devices.
+        /// </summary>
         private XBee64BitAddress xbee64address;
 
-        public XbeeSerialCommunication(XBee64BitAddress address)
+        /// <summary>
+        /// Connection state of this devices.
+        /// </summary>
+        private bool connected;
+
+        /// <summary>
+        /// The <see cref="XbeeNetwork" /> this devices belongs to.
+        /// </summary>
+        private XbeeNetwork network;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XbeeSerialCommunication" /> class.
+        /// Only for use through the <see cref="XbeeNetwork" /> class.
+        /// </summary>
+        /// <param name="address">The 64-bit address of this device.</param>
+        /// <param name="xbeeNetwork">The network this devices belongs to.</param>
+        internal XbeeSerialCommunication(XBee64BitAddress address, XbeeNetwork xbeeNetwork)
         {
             this.xbee64address = address;
+            this.network = xbeeNetwork;
         }
 
+        /// <inheritdoc/>
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        /// <inheritdoc/>
+        public event EventHandler Failure;
+
+        /// <inheritdoc/>
+        public event EventHandler ConnectionStateChanged;
+
+        /// <inheritdoc/>
+        public bool Connected { get => this.connected; }
+
+        /// <summary>
+        /// Gets the 64-bit address of the device.
+        /// </summary>
+        public XBee64BitAddress Xbee64address { get => this.xbee64address; }
+
+        /// <summary>
+        /// Closes the communication channel.
+        /// Notifies the <see cref="XbeeNetwork" /> of device closure.
+        /// </summary>
         public void Close()
         {
-            throw new NotImplementedException();
+            this.network.CloseXbeeDevice(this.xbee64address);
         }
 
+        /// <inheritdoc/>
         public void Write(byte[] input)
         {
-            throw new NotImplementedException();
+            // 0xFFFE means '16-bit address unknown or broadcast'
+            // We don't care about the 16-bit address here, since we use the 64-bit address, which is hardware-bound.
+            XBee16BitAddress x16a = new XBee16BitAddress("FFFE");
+            /*
+             Arg 1: Frame ID 0x01 = We do want feedback about the transmission
+             Arg 2 and 3: The 64-bit address and the 16-bit dummy address
+             Arg 4: Broadcast radius. Set to 0, but irrelevant because we are not sending broadcast messages.
+             Arg 5: Transmit options. Set to 0, irrelevant.
+             Arg 6: The actual data to transmit.
+            */
+            TransmitPacket tp = new TransmitPacket(0x01, this.xbee64address, x16a, 0x0, 0x0, input);
+            this.network.Write(tp.GenerateByteArrayEscaped());
+        }
+
+        /// <summary>
+        /// Processes data received from this <c>Xbee</c> module as parsed by the <see cref="XbeeNetwork" /> 
+        /// </summary>
+        /// <param name="receivedRfData">The data that was received, stripped from any <c>Xbee</c> API data</param>
+        internal void OnDataReceived(byte[] receivedRfData)
+        {
+            this.DataReceived?.Invoke(this, new DataReceivedEventArgs(receivedRfData));
+        }
+
+        /// <summary>
+        /// Notifies the user of this communication channel of changes in connection states of either the
+        /// serial communication channel that this channel is connected to or <c>Xbee</c> devices connection state (association or disassociation
+        /// from the network)
+        /// </summary>
+        /// <param name="connectionState">The connection state of the channel.</param>
+        internal void OnConnectionStateChanged(bool connectionState)
+        {
+            this.ConnectionStateChanged?.Invoke(this, new EventArgs());
+            this.connected = connectionState;
+        }
+
+        /// <summary>
+        /// Notifies the user of this communication channel about failures detected by the network.
+        /// Might be serial port failure, but might also be communication failure.
+        /// </summary>
+        internal void OnFailure()
+        {
+            this.Failure?.Invoke(this, new EventArgs());
         }
     }
 }
