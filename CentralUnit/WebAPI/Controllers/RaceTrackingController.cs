8668c78a6b8488a0d976aa441d21c03940f211c3
CptMalfunction <jan709.psn@gmail.com>
2021-03-07 13:39:47 +0100

    Issue-15: Added simulation race tracker to startup. Added endpoint fo tracker state

---
+++ CentralUnit/WebAPI/Controllers/RaceTrackingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using Newtonsoft.Json.Linq;
+using RaceManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RaceTrackingController : ControllerBase
+    {
+        private readonly ILogger<RaceTrackingController> logger;
+        private readonly RaceTracker tracker;
+
+        public RaceTrackingController(ILogger<RaceTrackingController> logger, RaceTracker tracker)
+        {
+            this.logger = logger;
+            this.tracker = tracker;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            JObject result = new JObject();
+            (List<EnteredEvent> waiting, List<(EnteredEvent id, TimingEvent timer)> onTrack, List<LeftEvent> unmatchedIds, List<TimingEvent> unmatchedTimes) = tracker.GetState;
+
+
+            result["waiting"] = JArray.FromObject(waiting);
+            result["onTrack"] = JArray.FromObject(onTrack);
+            result["unmatchedEndIds"] = JArray.FromObject(unmatchedIds);
+            result["unmatchedEndTimes"] = JArray.FromObject(unmatchedTimes);
+
+            return new JsonResult(result);
+        }
+    }
+}
--- CentralUnit/WebAPI/Startup.cs
+++ CentralUnit/WebAPI/Startup.cs
@@ -6,9 +6,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Models;
+using RaceManagement;
+using SensorUnits.RiderIdUnit;
+using SensorUnits.TimingUnit;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI
@@ -26,6 +32,39 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            RaceSummary race;
+            using (Stream input = new FileStream("D:\\Summary.json", FileMode.Open))
+                race = RaceSummary.ReadSummary(input);
+
+            SimulationRiderIdUnit startId = new SimulationRiderIdUnit(true, race);
+            SimulationRiderIdUnit endId = new SimulationRiderIdUnit(false, race);
+            SimulationTimingUnit timing = new SimulationTimingUnit(race);
+
+            startId.Initialize();
+            endId.Initialize();
+            timing.Initialize();
+
+            RaceTracker tracker = new RaceTracker(timing, startId, endId, timing.StartId, timing.EndId);
+
+            tracker.OnRiderFinished += (o, e) => Console.WriteLine($"Rider {e.Finish.Rider.Name} finished with a lap time of {e.Finish.LapTime} microseconds");
+            tracker.OnRiderDNF += (o, e) => Console.WriteLine($"Rider {e.Dnf.Rider.Name} did not finish since {e.Dnf.OtherRider.Rider.Name} finshed before them");
+            tracker.OnRiderWaiting += (o, e) => Console.WriteLine($"Rider {e.Rider.Rider.Name} can start");
+            tracker.OnStartEmpty += (o, e) => Console.WriteLine("Start box is empty");
+
+            CancellationTokenSource source = new CancellationTokenSource();
+
+            var trackTask = tracker.Run(source.Token);
+
+            var startTask = startId.Run(source.Token);
+            var endTask = endId.Run(source.Token);
+            var timeTask = timing.Run(source.Token);
+
+            var unitsTask = Task.WhenAll(startTask, endTask, timeTask);
+
+            services.AddSingleton<RaceTracker>(tracker);
+
+            services.AddMvc().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
