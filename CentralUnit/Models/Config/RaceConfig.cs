6fcdaf7e8b18c58e05efd4788c9e6fe7735e9b38
CptMalfunction <jan709.psn@gmail.com>
2021-05-01 12:44:17 +0200

    Issue #49: Made all fields in the Config request required

--- CentralUnit/Models/Config/RaceConfig.cs
+++ CentralUnit/Models/Config/RaceConfig.cs
@@ -1,20 +1,29 @@
-﻿namespace Models.Config
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.Config
 {
     public class RaceConfig
     {
+        [JsonRequired]
         public string TimingUnitId { get; set; }
+        [JsonRequired]
         public string StartIdUnitId { get; set; }
+        [JsonRequired]
         public string EndIdUnitId { get; set; }
         public int StartTimingGateId { get; set; }
+        [JsonRequired]
         public int EndTimingGateId { get; set; }
         /// <summary>
         /// The maximum distance in meters where a bluetooth singal is considered in range of the start id unit
         /// </summary>
         public double StartIdRange { get; set; }
+        [JsonRequired]
         /// <summary>
         /// The maximum distance in meters where a bluetooth singal is considered in range of the end id unit
         /// </summary>
         public double EndIdRange { get; set; }
+        [JsonRequired]
         /// <summary>
         /// The maximum time in seconds there may be between and end time event and an end id event to be matched
         /// </summary>
